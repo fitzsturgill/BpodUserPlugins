e52b1ceb2c1facdb448e773115bf93c989196a38
fritzPC2\Adam <fitzsturgill@gmail.com>
2017-04-07 13:31:42 -0400

    debugging nidaq issue where in instrumental version, no data is read off of card.  I think this has to do with matlab proceeding before being able to read off the data.  so I've uncommented the pause and stop call in processPhotometryAcq

--- Photometry/processPhotometryAcq.m
+++ Photometry/processPhotometryAcq.m
@@ -2,12 +2,12 @@ function processPhotometryAcq(currentTrial)
     global BpodSystem nidaq
     
     
-%     pause(0.05); 
-%     nidaq.session.stop() % Kills ~0.002 seconds after state matrix is done.
+    pause(0.05); 
+    nidaq.session.stop() % Kills ~0.002 seconds after state matrix is done.
 %     wait(nidaq.session) % Trying to wait until session is done - did we record the full session?
     
     % ensure outputs reset to zero
-%     nidaq.session.outputSingleScan(zeros(1,length(nidaq.aoChannels)));
+    nidaq.session.outputSingleScan(zeros(1,length(nidaq.aoChannels)));
 
     %% Save data in BpodSystem format.
     BpodSystem.Data.NidaqData{currentTrial, 1} = nidaq.ai_data; %input data
