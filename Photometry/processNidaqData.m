7663f911dc3d348ebef61a11cedb10e92c9138ed
fritzPC2\Adam <fitzsturgill@gmail.com>
2017-06-15 16:55:19 -0400

    troubleshooting

--- Photometry/processNidaqData.m
+++ Photometry/processNidaqData.m
@@ -13,10 +13,18 @@ function processNidaqData(src,event)
         nidaq.ai_data = nidaq.ai_data(1:correctSamples, :);
     end
 
-
-%     pause(0.05); % wait for hardware to stop, see error message below, I think this addresses the below error message:
+% 
+%  % wait for hardware to stop, see error message below, I think this addresses the below error message:
 % %     Error using processNidaqData (line 15)
 % % Internal Error: The hardware did not report that it stopped before the timeout elapsed.
-%     nidaq.session.stop(); % Kills ~0.002 seconds after state matrix is done.
-%     wait(nidaq.session);
-%     nidaq.session.outputSingleScan([0 0]); % make sure LEDs are turned off
+    nidaq.session.stop(); % Kills ~0.002 seconds after state matrix is done.
+    while nidaq.session.IsRunning
+        pause(0.005);
+        disp('waiting for session stop');
+    end
+    nidaq.session.outputSingleScan([0 0]); % make sure LEDs are turned off
+    disp('flushing data');
+    pause(0.005);
+    nidaq.session.outputSingleScan([0 0]); % make sure LEDs are turned off 
+    pause(0.005);
+    nidaq.session.outputSingleScan([0 0]); % make sure LEDs are turned off 
--- Photometry/stopPhotometryAcq.m
+++ Photometry/stopPhotometryAcq.m
@@ -3,9 +3,18 @@ function stopPhotometryAcq
     % output data 
     global nidaq
     
-    pause(0.05); % wait for hardware to stop, see error message below, I think this addresses the below error message:
-    %     Error using processNidaqData (line 15)
-    % Internal Error: The hardware did not report that it stopped before the timeout elapsed.
+%     pause(0.05); % wait for hardware to stop, see error message below, I think this addresses the below error message:
+%     %     Error using processNidaqData (line 15)
+%     % Internal Error: The hardware did not report that it stopped before the timeout elapsed.
     nidaq.session.stop(); % Kills ~0.002 seconds after state matrix is done.
-    wait(nidaq.session);
+%     wait(nidaq.session);
+    while nidaq.session.IsRunning
+        pause(0.005);
+        disp('waiting for session stop');
+    end
     nidaq.session.outputSingleScan([0 0]); % make sure LEDs are turned off
+    disp('flushing data');
+    pause(0.005);
+    nidaq.session.outputSingleScan([0 0]); % make sure LEDs are turned off 
+    pause(0.005);
+    nidaq.session.outputSingleScan([0 0]); % make sure LEDs are turned off 
